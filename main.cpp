956a8a7165b62f78485bdb442bde8ea50d5e215e
Maurice Huang <mauricehuang9@gmail.com>
2021-12-03 16:55:07 +0800

    Initialize project

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+    vector<int> symbols = {1,2,3,4,5,6};
+
+    cout << symbols[3];
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,3 @@
+all: main.cpp
+	@g++-10 -std=c++11 main.cpp
+	@./a.out
